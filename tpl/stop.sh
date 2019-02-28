567ea4bbb5e107a483faf90c2c9b7c07af2d7263
HaoXuan40404 <444649358@qq.com>
2019-02-28 16:26:07 +0800

    modify expand

--- tpl/stop.sh
+++ tpl/stop.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 SHELL_FOLDER=$(cd $(dirname $0);pwd)
-node=$(basename ${SHELL_FOLDER})
+
 fisco_bcos=${SHELL_FOLDER}/fisco-bcos
-weth_pid=`ps aux|grep "${fisco_bcos}"|grep -v grep|awk '{print $2}'`
-kill_cmd="kill ${weth_pid}"
-if [ ! -z $weth_pid ];then
-    echo " stop ${node} ..."
-    eval ${kill_cmd}
-else
-    echo " ${node} is not running."
-fi
+node=$(basename ${SHELL_FOLDER})
+node_pid=`ps aux|grep "${fisco_bcos}"|grep -v grep|awk '{print $2}'`
+try_times=2
+i=0
+while [ $i -lt ${try_times} ]
+do
+    if [ -z ${node_pid} ];then
+        echo " ${node} isn't running."
+        exit 0
+    fi
+    [ ! -z ${node_pid} ] && kill ${node_pid}
+    sleep 1
+    node_pid=`ps aux|grep "${fisco_bcos}"|grep -v grep|awk '{print $2}'`
+    if [ -z ${node_pid} ];then
+        echo " stop ${node} success."
+        exit 0
+    fi
+    ((i=i+1))
+done
+
--- tpl/stop_all.sh
+++ tpl/stop_all.sh
@@ -3,7 +3,12 @@ SHELL_FOLDER=$(cd $(dirname $0);pwd)
 
 for directory in `ls ${SHELL_FOLDER}`  
 do  
-    if [ -d "${SHELL_FOLDER}/${directory}" ];then  
-        bash ${SHELL_FOLDER}/${directory}/stop.sh
+    if [[ -d "${SHELL_FOLDER}/${directory}" && -f "${SHELL_FOLDER}/${directory}/stop.sh" ]];then  
+        echo "stop ${directory}" && bash ${SHELL_FOLDER}/${directory}/stop.sh
     fi  
-done  
+done
+sleep 2
+
+        # if [ "{directory}" == "monitor" ];then
+        #     continue;
+        # fi
