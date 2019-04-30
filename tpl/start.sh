2953a8bf0e7c979bd52d2c808c1c68867894dd46
HaoXuan40404 <444649358@qq.com>
2019-04-30 15:27:21 +0800

    fix guomi group2 rlp

--- tpl/start.sh
+++ tpl/start.sh
@@ -17,7 +17,7 @@ try_times=4
 i=0
 while [ $i -lt ${try_times} ]
 do
-    node_pid=`ps aux|grep ${fisco_bcos}|grep -v grep|awk '{print $2}'`
+    node_pid=$(ps aux|grep ${fisco_bcos}|grep -v grep|awk '{print $2}')
     if [ ! -z ${node_pid} ];then
         echo -e "\033[32m ${node} start successfully\033[0m"
         exit 0
