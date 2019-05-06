df9eaa133d158311858b98198d4a01134c6e37ea
HaoXuan40404 <444649358@qq.com>
2019-05-06 14:57:06 +0800

    fix python and pip difference

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,10 @@
 # @ file     : install.sh
 # @ date     : 2019
 
-SHELL_FOLDER=$(cd $(dirname $0);pwd)
+SHELL_FOLDER=$(
+    cd $(dirname $0)
+    pwd
+)
 project_dir="${SHELL_FOLDER}/.."
 # use_cores=1
 # test_mode=0
@@ -16,42 +19,37 @@ Centos_Platform=1
 # Macos_Platform=2
 
 # check sudo permission
-function sudo_permission_check() 
-{
+sudo_permission_check() {
     # sudo echo -n " "
 
     # if [ $? -ne 0 ]; then
-    if ! sudo echo -n " "
-    then
-        echo "no sudo permission, please add youself in the sudoers"; exit 1;
+    if ! sudo echo -n " "; then
+        echo "no sudo permission, please add youself in the sudoers"
+        exit 1
     fi
 }
 
-clear_cache()
-{ 
+clear_cache() {
     cd ${project_dir}
     execute_cmd "rm -rf deps/src/*stamp"
 }
 
-LOG_ERROR()
-{
+LOG_ERROR() {
     content=${1}
     echo -e "\033[31m${content}\033[0m"
 }
 
-LOG_INFO()
-{
+LOG_INFO() {
     content=${1}
     echo -e "\033[32m${content}\033[0m"
 }
 
-execute_cmd()
-{
+execute_cmd() {
     command="${1}"
     #LOG_INFO "RUN: ${command}"
     eval ${command}
     ret=$?
-    if [ $ret -ne 0 ];then
+    if [ $ret -ne 0 ]; then
         LOG_ERROR "FAILED execution of command: ${command}"
         # clear_cache
         exit 1
@@ -61,49 +59,59 @@ execute_cmd()
 }
 
 # get platform: now support debain/ubuntu, fedora/centos, oracle
-get_platform()
-{
-    uname -v > /dev/null 2>&1 || { echo >&2 "ERROR - Require 'uname' to identify the platform."; exit 1; }
+get_platform() {
+    uname -v >/dev/null 2>&1 || {
+        echo >&2 "ERROR - Require 'uname' to identify the platform."
+        exit 1
+    }
     case $(uname -s) in
     Darwin)
         LOG_ERROR "Not Support mac OS Yet!"
-        exit 1;;
+        exit 1
+        ;;
     FreeBSD)
         LOG_ERROR "Not Support FreeBSD Yet!"
-        exit 1;;
+        exit 1
+        ;;
     Linux)
-        if [ -f "/etc/arch-release" ];then
+        if [ -f "/etc/arch-release" ]; then
             LOG_ERROR "Not Support arch-linux Yet!"
-        elif [ -f "/etc/os-release" ];then
-            DISTRO_NAME=$(. /etc/os-release; echo $NAME)
+        elif [ -f "/etc/os-release" ]; then
+            DISTRO_NAME=$(
+                . /etc/os-release
+                echo $NAME
+            )
             case $DISTRO_NAME in
-            Debian*|Ubuntu)
+            Debian* | Ubuntu)
                 LOG_INFO "Debian*|Ubuntu Platform"
-                return ${Ubuntu_Platform};; #ubuntu type
-            Fedora|CentOS*)
+                return ${Ubuntu_Platform}
+                ;; #ubuntu type
+            Fedora | CentOS*)
                 LOG_INFO "Fedora|CentOS* Platform"
-                return ${Centos_Platform};; #centos type
+                return ${Centos_Platform}
+                ;; #centos type
             Oracle*)
                 LOG_INFO "Oracle Platform"
-                return ${Centos_Platform};; #oracle type
+                return ${Centos_Platform}
+                ;; #oracle type
             esac
         else
             LOG_ERROR "Unsupported Platform"
         fi
+        ;;
     esac
 }
 
-install_all_deps()
-{
+install_all_deps() {
     get_platform
     platform=$(echo $?)
     # platform=$(get_platform)
-    if [ ${platform} -eq ${Ubuntu_Platform} ];then
+    if [ ${platform} -eq ${Ubuntu_Platform} ]; then
         sudo apt-get -y install python-pip
         sudo apt-get -y install curl
         sudo apt-get -y install openssl
         # sudo apt-get -y install nc
-    elif [ ${platform} -eq ${Centos_Platform} ];then
+    elif [ ${platform} -eq ${Centos_Platform} ]; then
         sudo yum install -y python-pip
         sudo yum -y install openssl
         # sudo yum -y install nc
@@ -114,25 +122,48 @@ install_all_deps()
     fi
 }
 
-install_deps()
-{
+install_deps() {
     sudo_permission_check
     install_all_deps
 }
 
-install_all()
-{
+install_all() {
     install_deps
     pip install configparser --user
-    py_version=$($python_env -V 2>&1 | awk {'print $2'} | awk -F. {' print $1"."$2"."$3 '})
-    # py_pip=$(pip -V 2>&1 | awk {'print $2'} | awk -F. {' print $1"."$2"."$3 '})
+    py_version=$($python_env -V 2>&1 | awk {'print $2'} | awk -F. {' print $1 '})
+    py_pip=$(pip -V 2>&1 | awk 'END {print $6}' | sed 's/)$//' | awk -F. '{print $1}')
 
     # params check
-    if [ -z "${py_version}" ];then
-        alarm " not invalid python path, path is ${python_env}."; exit 1;
+    if [ -z "${py_version}" ]; then
+        alarm " not invalid python path, path is ${python_env}."
+        exit 1
     fi
+
+    if [ ${py_version} != ${py_pip} ]; then
+     alarm "python and pip is not same version"
+     alarm "python -V"
+     python -v
+     alarm "pip -V"
+     pip -v
+     exit 1
+    fi
+
+    if [ ${py_pip} == "3" ]; then
+     python_local=$(which python)
+     echo "try to use python3"
+     if [ ! -x "./generator"]; then 
+        sed -i 's?#!/usr/bin/python?#!${python_local}?' ./generator
+     elif [ ! -x "../generator"]; then 
+        sed -i 's?#!/usr/bin/python?#!${python_local}?' ./generator
+     else
+        alarm "Cann't find generator!"
+        exit 1
+     fi
+    fi
+
+
 }
-if [ "$1" == "deps" ];then
+if [ "$1" == "deps" ]; then
     install_deps
 else
     install_all
