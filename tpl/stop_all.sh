9cc3f26e39ad77b945b18e5345c19c875759c968
HaoXuan40404 <444649358@qq.com>
2019-05-21 13:32:21 +0800

    update tpl for rc3

--- tpl/stop_all.sh
+++ tpl/stop_all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 SHELL_FOLDER=$(cd $(dirname $0);pwd)
 
-for directory in $(ls ${SHELL_FOLDER})
+dirs=($(ls -l ${SHELL_FOLDER} | awk '/^d/ {print $NF}'))
+for directory in ${dirs[*]}  
 do  
     if [[ -d "${SHELL_FOLDER}/${directory}" && -f "${SHELL_FOLDER}/${directory}/stop.sh" ]];then  
         echo "try to stop ${directory}"
         bash ${SHELL_FOLDER}/${directory}/stop.sh &
     fi  
-done
+done  
 sleep 3
