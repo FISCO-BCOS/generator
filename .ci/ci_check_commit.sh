8dcc870132af7dc8b8462f3bbb3994d7040cb0a0
HaoXuan40404 <444649358@qq.com>
2019-08-19 18:45:22 +0800

    remove scan at ci

--- .ci/ci_check_commit.sh
+++ .ci/ci_check_commit.sh
@@ -49,7 +49,7 @@ scan_code()
 {
     # Redirect output to stderr.
     exec 1>&2
-    for file in $(git diff-index --name-status ${against} | awk '{print $2}'); do
+    for file in $(git diff-index --name-status ${against} | grep -v .ci | awk '{print $2}'); do
         execute_cmd "${scan_code_script} $file -f json -o /tmp/report.json"
         trigger_rules=$(jq -r '.' /tmp/report.json | grep 'trigger_rules' | awk '{print $2}' | sed 's/,//g')
         echo "trigger_rules is ${trigger_rules}"
