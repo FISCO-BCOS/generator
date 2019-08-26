7de4892931f2b65e606cabea9e128b467601c665
HaoXuan40404 <444649358@qq.com>
2019-08-26 11:07:40 +0800

    rename sdk.crt

--- .ci/download_bin.sh
+++ .ci/download_bin.sh
@@ -2,31 +2,31 @@
 
 try_download_gmfisco() {
     echo "download fisco in travis ci!"
-    ./generator --download_fisco ./meta -g
     while [ ! -f "./meta/fisco-bcos" ]
     do
     ./generator --download_fisco ./meta -g
+    # bash <(curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/master/tools/ci/download_bin.sh) -b dev -g
+    # mv ./bin/fisco-bcos ./meta/fisco-bcos
     done
 }
 
 try_download_fisco() {
     echo "download fisco in travis ci!"
-    ./generator --download_fisco ./meta
     while [ ! -f "./meta/fisco-bcos" ]
     do
     ./generator --download_fisco ./meta
+    # bash <(curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/master/tools/ci/download_bin.sh) -b dev
+    # mv ./bin/fisco-bcos ./meta/fisco-bcos
     done
 }
 
 try_download_console() {
     echo "download fisco in travis ci!"
-    ./generator --download_console ./meta
     while [ ! -d "./meta/console" ]
     do
     ./generator --download_console ./meta
     done
 }
-
 
 case "$1" in
 -g)
